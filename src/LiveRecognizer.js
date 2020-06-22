0e8bbc1f0c8b85f662fb6031b7e3d4bcd6883dd8
Arvid Melinder <43202769+Mellas84@users.noreply.github.com>
2020-06-22 10:45:20 +0200

    reorderd images

--- src/LiveRecognizer.js
+++ src/LiveRecognizer.js
@@ -12,7 +12,7 @@ function loadLabeledImages() {
       const descriptions = [];
       let nrImages =
       label === "Arvid" ? 17
-      : label === "Max" ? 1
+      : label === "Max" ? 3
       : 1;
       for (let i = 1; i <= nrImages; i++) {
         const img = await faceapi.fetchImage(
